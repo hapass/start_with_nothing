59976f6023e6090923122fa6f2a9551c8223d4b3
Pavel Zavoiskikh <hapass@ya.ru>
2020-03-11 18:21:09 +0300

    Move shininness to fragment shader. This smoothens the shining circle.

--- src/main/glsl/engine/graphics/rendering/FragmentShader.glsl
+++ src/main/glsl/engine/graphics/rendering/FragmentShader.glsl
@@ -1,7 +1,16 @@
 precision mediump float;
 
 varying vec4 color;
+uniform vec3 glow_position;
+uniform mat4 projection;
 
 void main() {
-    gl_FragColor = color;
+    vec2 shine_position = gl_FragCoord.xy - vec2(glow_position.x, 600.0 - glow_position.y);
+    vec2 frag_coord = vec2(shine_position.x / 800.0, shine_position.y / 600.0);
+    float shininess = max(1.0 - dot(frag_coord, frag_coord) * glow_position.z, 0.0);
+    if (glow_position.z > 14.9)
+    {
+        shininess = 0.0;
+    }
+    gl_FragColor = color * shininess;
 }
