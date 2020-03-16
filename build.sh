f8e3ce3fb46261ac716ed8f8ca6b4b7202a188a2
Pavel Zavoiskikh <hapass@ya.ru>
2020-03-16 03:21:26 +0300

    Minify js in release build.

--- build.sh
+++ build.sh
@@ -5,12 +5,16 @@ else
 fi
 
 mkdir -p bin
+rm -rf bin/*
 cp layout/index.html bin/index.html
 
 if [ "$configuration" == "debug" ]; then
     haxe build.hxml -debug
 else
     haxe build.hxml
+    cd bin
+    curl -X POST -s --data-urlencode 'input@glow.js' https://javascript-minifier.com/raw > glow.min.js
+    cd ..
     neko replace.n
     cp bin/index.html index.html
 fi
--- src/build/haxe/replace/Main.hx
+++ src/build/haxe/replace/Main.hx
@@ -1,13 +1,14 @@
 package replace;
 
 import sys.io.File;
+import haxe.Http;
 
 using StringTools;
 
 class Main {
-  static function main() {
-      File.saveContent('bin/index.html', File.getContent('bin/index.html').replace(
+    static function main() {
+        File.saveContent('bin/index.html', File.getContent('bin/index.html').replace(
         '<script type="text/javascript" src="glow.js"></script>',
-        '<script type="text/javascript">${File.getContent('bin/glow.js')}</script>'));
-  }
+        '<script type="text/javascript">${File.getContent('bin/glow.min.js')}</script>'));
+    }
 }
